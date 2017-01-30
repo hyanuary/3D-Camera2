9d0a1f50a6fc357791d153f07e45234de3911eb2
Henoch Yanuary <1003495@student.sae.edu.au>
2017-01-31 00:25:07 +0900

    zoom code being applied on camera 3rd

--- Assets/CameraThird.cs
+++ Assets/CameraThird.cs
@@ -8,6 +8,16 @@ public class CameraThird : MonoBehaviour {
 	public float rotateSpeed = 5;
 	Vector3 offset;
 
+	//zoomin variable 1
+	public float curZoomPos;
+	public float zoomTo;
+	public float zoomFrom = 20.0f;
+
+	//zoomin variable 2
+	public float minPoint = 15.0f;
+	public float maxPoint = 90.0f;
+	public float sensivity = 10.0f;
+
 	void Start() {
 		offset = (player.transform.position - transform.position) * offsetMultiplier;
 	}
@@ -21,6 +31,10 @@ public class CameraThird : MonoBehaviour {
 		transform.position = player.transform.position - (rotation * offset);
 
 		transform.LookAt(player.transform);
+
+		//ZoomFactor ();
+
+		ZoomFactor2 ();
 
 		SpeedFactor ();
 	}
@@ -46,5 +60,33 @@ public class CameraThird : MonoBehaviour {
 			offsetMultiplier = 1;
 		if (offsetMultiplier > 1.4f)
 			offsetMultiplier = 1.4f;
+	}
+
+	void ZoomFactor()
+	{
+		float y = Input.mouseScrollDelta.y;
+
+		if (y >= 1) {
+			zoomTo -= 5.0f;
+		} 
+		else if (y >= -1) {
+			zoomTo += 5.0f;
+		}
+
+		curZoomPos = zoomFrom + zoomTo;
+
+		curZoomPos = Mathf.Clamp (curZoomPos, 5.0f, 35.0f);
+		zoomTo = Mathf.Clamp (zoomTo, -15.0f, 30.0f);
+
+		Camera.main.fieldOfView = curZoomPos;
+
+	}
+
+	void ZoomFactor2()
+	{
+		float fov = Camera.main.fieldOfView;
+		fov += Input.GetAxis ("Mouse ScrollWheel") * sensivity;
+		fov = Mathf.Clamp (fov, minPoint, maxPoint);
+		Camera.main.fieldOfView = fov;
 	}
 }
